5be46434eda7afdcb7912cf7995b0502d406f61e
Luke Gartland <lukegartland@gmail.com>
2021-07-24 07:42:27 -1000

    players and balance data added

--- src/App.js
+++ src/App.js
@@ -3,23 +3,39 @@ import "./App.css";
 import React from "react";
 // import web3 from './web3'
 import lottery from './lottery'
+import web3 from './web3';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { manager: '' }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = { manager: '' }
+  // }
+  state = {
+    manager: '',
+    players: [],
+    balance: ''
   }
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call()
-    this.setState({ manager })
+    const players = await lottery.methods.getPlayers().call()
+    const balance = await web3.eth.getBalance(lottery.options.address)
+
+    this.setState({ manager, players, balance })
+
   }
   render() {
    
     return (
      <div>
        <h2>Lottery Contract</h2>
-       <p>This contract is managed by {this.state.manager}</p>
+       <p>
+         This contract is managed by {this.state.manager}
+       </p>
+      <p>
+      There are currently {this.state.players.length} players entered, competing to win
+         {web3.utils.fromWei(this.state.balance, 'ether')} Ether!
+      </p>
      </div>
     );
   }
