e08139b4e3d72f421a7dade513066e3ebe097fef
Luke Gartland <lukegartland@gmail.com>
2021-07-24 07:15:21 -1000

    web3 connection to deployed contract

---
+++ src/lottery.js
@@ -0,0 +1,7 @@
+import web3 from './web3';
+
+const address = '0xB4c1E51FE8805A7142fB15dB4245bd5515091A45'
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+export default new web3.eth.Contract(abi, address);
